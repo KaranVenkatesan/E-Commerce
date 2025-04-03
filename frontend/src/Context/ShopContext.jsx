512884002bb9624191b7afa53397c7f0156ecf61
KaranVenkatesan <vkaranraina@gmail.com>
2025-04-03 13:17:17 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -23,7 +23,7 @@ const ShopContextProvider = (props) => {
 
         if (localStorage.getItem('auth-token')) {
             fetch(`${BACKEND_URL}/getcart`, {
-                method: 'POST','GET'
+                method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'auth-token': localStorage.getItem('auth-token'),
